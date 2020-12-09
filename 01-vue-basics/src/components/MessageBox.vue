f2eb499d2d91cecd23da3bb4ca1adb30131d8212
yuhengen <yuhengen@gmail.com>
2020-12-09 07:54:11 +0000

    Initial commit

---
+++ 01-vue-basics/src/components/MessageBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 v-bind:style="{ 'backgroundColor': bgcolor }">{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      messages: "The quick brown fox jumps over the lazy dog",
+    };
+  },
+  // props act like arguments
+  props: ["message", "bgcolor"],
+};
+</script>
+
+<style scoped>
+div {
+  background-color: pink;
+  text-align: center;
+}
+</style>
